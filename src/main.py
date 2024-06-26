e4d3c5d334bc2ae1947582f5fcc1afe2f239b256
Andy <andy@impv.au>
2024-06-26 14:34:29 +1000

    include production of our outputs for the paper

--- src/main.py
+++ src/main.py
@@ -6,3 +6,23 @@ from preproc import *
 download(dataDir)
 dfTrain, dfTest = preproc(dataDir)
 
+# Filter according to the paper, generated the mean
+dfTrainPaper = f_all_paper(dfTrain)
+dfTrainOurs = f_all_ours(dfTrain)
+dfTestPaper = f_all_paper(dfTest, False)
+dfTestOurs = f_all_ours(dfTest, False)
+
+# For speed, commented out here
+dfTrainPaper.to_csv( f"{outDataDir}/train_paper.csv", index=False)
+dfTestPaper.to_csv(f"{outDataDir}/test_paper.csv", index=False)
+dfTrainOurs.to_csv(f"{outDataDir}/train_ours.csv", index=False)
+dfTestOurs.to_csv(f"{outDataDir}/test_ours.csv", index=False)
+
+# We create a subset of the data with only the key fields, and we rename those fields appropriately
+dfTrainOurs_filtered = dfTrainOurs.loc[:, iConsider]
+dfTrainOurs_filtered.columns = iRename
+dfTrainOurs_filtered.to_csv(f"{outDataDir}/train_ours_min.csv", index=False)
+
+dfTestOurs_filtered = dfTestOurs.loc[:, iConsider]
+dfTestOurs_filtered.columns = iRename
+dfTestOurs_filtered.to_csv(f"{outDataDir}/test_ours_min.csv", index=False)
